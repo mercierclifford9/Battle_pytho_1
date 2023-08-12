13c7d9301fdd6da08af294fa8a0f531c84ad6dca
mercierclifford9 <140321835+mercierclifford9@users.noreply.github.com>
2023-08-12 19:33:59 -0400

    Add files via upload

--- battle_python_1.py
+++ battle_python_1.py
@@ -4,9 +4,11 @@ y=[]
 for i in x:
     dijitt=re.findall(r'\d+', x)
     dijitt=[int(dijit) for dijit in dijitt]
-
+h=""
 print("dijit yo se :")
 for i in dijitt:
-    print(i)
-nb_val=len(dijitt)
+    h=h+str(i)
+nb_val=len(h)
+print(h)
 print("kantite dijit yo se :" + str(nb_val))
+print()
